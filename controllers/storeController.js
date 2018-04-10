3c43953e66c50813564ce96991e53da225319cee
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-04-10 18:25:38 +0300

    Update existing store using the same template as for add

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -29,7 +29,7 @@ exports.createStore = async (req, res) => {
   await store.save();
   console.log('Store saved!')
   req.flash('success', `Successfully created ${store.name}! Care to leave a review?`)
- res.redirect(`/store/${store.slug}`);
+  res.redirect(`/store/${store.slug}`);
 }
 
 exports.getStores = async (req, res) => {
@@ -39,4 +39,29 @@ exports.getStores = async (req, res) => {
     title: 'Stores',
     stores,
   });
+}
+
+exports.editStore = async (req, res) => {
+  // This is called when we pass store id as a route param.
+  // The template rendered is the same as for addStore() controller,
+  // but here we have data (store by id) which affects the title of the page
+  // and lets populate fields in storeForm mixin.
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', {
+    title: `Edit ${store.name}`,
+    store
+  });
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({
+      _id: req.params.id
+    },
+    req.body,
+    { 
+      new: true, // return the new store instead of old one
+      runValidators: true
+    }).exec();
+  req.flash('success', `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View store</a>`);
+  res.redirect(`/stores/${store._id}/edit`); 
 }
--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,7 @@ router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
-
-router.get('/reverse/:name', function(req, res) {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
